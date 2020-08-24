6dda0d05db3b20a8776d152081f4f07d7a52675c
jb897 <beachbum.bowman@gmail.com>
2020-08-24 15:48:54 -0400

    Updates

---
+++ precache-manifest.46d23de4944f99d1a92a3b063880a021.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b7979a802744cba600fcb1e0833884",
+    "url": "/oldeworldremedies/index.html"
+  },
+  {
+    "revision": "cdecb79a2752c6185583",
+    "url": "/oldeworldremedies/static/css/main.74fea08e.chunk.css"
+  },
+  {
+    "revision": "e099cc657e8cdc5cfb5b",
+    "url": "/oldeworldremedies/static/js/2.2bf207de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/oldeworldremedies/static/js/2.2bf207de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdecb79a2752c6185583",
+    "url": "/oldeworldremedies/static/js/main.77189600.chunk.js"
+  },
+  {
+    "revision": "46ea2c2262ce8d31663c",
+    "url": "/oldeworldremedies/static/js/runtime-main.1b630878.js"
+  },
+  {
+    "revision": "7b3efbe4acb786358aae959dcb8ebf9d",
+    "url": "/oldeworldremedies/static/media/cannabis.7b3efbe4.jpg"
+  },
+  {
+    "revision": "cfc6f9fd4d2785f435cfc4059dfda43f",
+    "url": "/oldeworldremedies/static/media/cbd.cfc6f9fd.jpg"
+  },
+  {
+    "revision": "4f92517c92e710d7b72f9481d2ddb536",
+    "url": "/oldeworldremedies/static/media/concentrate.4f92517c.jpg"
+  },
+  {
+    "revision": "5a02cec7a91cc9cc3cdb3590c6722f46",
+    "url": "/oldeworldremedies/static/media/edible.5a02cec7.jpg"
+  },
+  {
+    "revision": "46f52057608a40c3bf49787c6393ccbb",
+    "url": "/oldeworldremedies/static/media/flower.46f52057.jpg"
+  },
+  {
+    "revision": "f3a9d5107484299cbc9c3d62cc057d5e",
+    "url": "/oldeworldremedies/static/media/preroll.f3a9d510.jpg"
+  },
+  {
+    "revision": "25a393f0bc0e65f0e47fc7eda4f42dc6",
+    "url": "/oldeworldremedies/static/media/profile.25a393f0.jpeg"
+  },
+  {
+    "revision": "58679b63a35eb234719ca676162e7f70",
+    "url": "/oldeworldremedies/static/media/vaporizer.58679b63.jpg"
+  }
+]);
